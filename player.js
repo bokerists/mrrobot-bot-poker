964b65d1a3833cab69d0d619249933363c0ab4b0
Davide Melfi <tech838@yoox.com>
2016-03-22 18:54:11 +0100

    Fourth try

--- player.js
+++ player.js
@@ -1,34 +1,42 @@
-
 exports = module.exports = {
 
-  VERSION: 'Superstar poker js-player',
+    VERSION: 'Superstar poker js-player',
 
-  bet: function (gamestate, bet) {
+    bet: function(gamestate, bet) {
 
-    //
-    // gamestate contains info about the state of the game.
-    // check the documentation at https://bot-poker.herokuapp.com/wiki for further info about the data structure.
+        //
+        // gamestate contains info about the state of the game.
+        // check the documentation at https://bot-poker.herokuapp.com/wiki for further info about the data structure.
 
-    //
-    // bet is the function you should use to send your bet.
-
-
-
-    // enjoy the game!
-
-    //
-    // currently we just fold every single hand.
-
-    'use strict';
+        //
+        // bet is the function you should use to send your bet.
 
 
-    if(player.cards[0].rank === player.cards[1].rank)
-      return bet(10);
 
-    console.log(`Currently playing tournament ${gamestate.tournamentId}`);
+        // enjoy the game!
 
-    return bet(0);
+        //
+        // currently we just fold every single hand.
 
-  }
+        'use strict';
+
+
+        if (player.cards[0].rank === player.cards[1].rank) {
+            if (isCardFigure(player.cards[0].rank)) {
+                return bet(10);
+            } else {
+                return bet(2);
+            }
+        }
+
+        console.log(`Currently playing tournament ${gamestate.tournamentId}`);
+
+        return bet(0);
+
+    },
+
+    isCardFigure: function(rank) {
+        return rank === 'A' || rank === 'K' || rank === 'Q' || rank === 'J';
+    }
 
 };
